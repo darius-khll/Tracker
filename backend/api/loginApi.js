9cf4ae6c022409422a38463f6a54d8fbc1e27b79
ali <alikhll@outlook.com>
2018-07-23 12:19:25 +0430

    login api enhanced

--- backend/api/loginApi.js
+++ backend/api/loginApi.js
@@ -1,14 +1,13 @@
-const express = require('express');
-const router = express.Router();
 
-router.get('/get/:id', (req, res) => {
-    let id = req.params.id;
-    if(id == 1)
-        throw new Error("error happens");
-    let response = {
-        id: id
-    };
-    res.end(JSON.stringify(response));
-});
+module.exports = (app) => {
+    app.get('/get/:id', (req, res) => {
+        let id = req.params.id;
+        if (id == 1)
+            throw new Error("error happens");
 
-module.exports = router;
+        let response = {
+            id: id
+        };
+        res.end(JSON.stringify(response));
+    });
+}
