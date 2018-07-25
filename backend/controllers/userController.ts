335373a7a26f5383fcf11964550e58e3d8fc265d
ali <alikhll@outlook.com>
2018-07-25 13:53:36 +0430

    userController dependency injection added

--- backend/controllers/userController.ts
+++ backend/controllers/userController.ts
@@ -1,16 +1,16 @@
 import * as express from "express";
 import { interfaces, controller, httpGet, httpPost, httpDelete, request, queryParam, response, requestParam } from "inversify-express-utils";
 import { injectable, inject } from "inversify";
-//import { UserService } from "../service/userService";
+import { UserService1, UserService } from "../service/userService";
 
 @controller("/user")
 export class UserController implements interfaces.Controller {
 
-    //constructor(@inject("UserService") private userService: UserService) { }
+    constructor(@inject("UserService") private userService: UserService) { }
 
     @httpGet("/")
     private index(req: express.Request, res: express.Response, next: express.NextFunction): void {
-        //this.userService.addUser1();
-        res.end("wow");
+        let a = this.userService.addUser();
+        res.end(a);
     }
 }
