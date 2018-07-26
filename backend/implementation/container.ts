002a787d78b3ce3e07da57903fc111209bf47287
ali <alikhll@outlook.com>
2018-07-26 12:19:05 +0430

    mongo context added to container

--- backend/implementation/container.ts
+++ backend/implementation/container.ts
@@ -1,8 +1,9 @@
 import * as srv from "../service/userService";
+import { Context } from "../model/context"
 import { Container } from "../node_modules/inversify";
 
 module.exports = (container: Container) => {
-    container.bind<srv.UserService1>('UserService1').to(srv.UserService1).inRequestScope();
+    container.bind<Context>('Context').to(Context).inRequestScope();
     container.bind<srv.UserService>('UserService').to(srv.UserService).inRequestScope();
 }
 
