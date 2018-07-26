81cf276665d3948985b86d0cc75958bc48aab920
ali <alikhll@outlook.com>
2018-07-26 12:20:03 +0430

    injectable decorator on context added

--- backend/model/context.ts
+++ backend/model/context.ts
@@ -1,11 +1,14 @@
 import * as mongoose from "mongoose"
-
+import { injectable, inject } from "inversify";
 let user = require("./user");
 
-export default class context {
-    public async connect() { await mongoose.connect('mongodb://localhost:8585/tracker'); }
-    public async dispose() { await mongoose.disconnect(); }
+@injectable()
+export class Context {
+    public async connect() { await mongoose.connect('mongodb://localhost:8585/tracker', { useNewUrlParser: true }); }
+    public async dispose() {
+        if (mongoose) await mongoose.disconnect();
+    }
 
     get user() { return user; }
-    
+
 }
