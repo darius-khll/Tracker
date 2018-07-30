128c3f4230e00852e410c28e764e7dd9e811c08c
ali <alikhll@outlook.com>
2018-07-30 15:26:11 +0430

    debugger removed

--- backend/implementation/log.ts
+++ backend/implementation/log.ts
@@ -2,7 +2,6 @@
 module.exports = (app : any) => {
     app.use((req : any, res: any, next: any) => {
         next();
-        debugger;
         if(res.statusCode == 404)
         {
             res.end("haha 404 ;)");
