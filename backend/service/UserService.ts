c0e8b8a209da511864790899eb279d198bd295e0
ali <alikhll@outlook.com>
2018-07-30 15:26:36 +0430

    user service constructor injection added

--- backend/service/UserService.ts
+++ backend/service/UserService.ts
@@ -4,18 +4,25 @@ import { Context } from "../model/context"
 
 @injectable()
 export class UserService implements IUserService {
-    @inject("Context") private context: Context
+    //@inject("Context") private context: Context
+
+    private _context: Context;
+    public constructor(
+        @inject("Context") context: Context,
+    ) {
+        this._context = context;
+    }
 
     public async addUser(): Promise<void> {
         try {
-            await this.context.connect();
-            const u = new this.context.user({ name: 'www' });
+            await this._context.connect();
+            const u = new this._context.user({ name: 'www1' });
             await u.save();
         }
         catch (e) {
             throw new Error(e);
         } finally {
-            await this.context.dispose();
+            await this._context.dispose();
         }
     }
 }
