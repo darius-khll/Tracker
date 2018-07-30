b536fd563bdbdfe48d1debba04c1a0094b9c1942
ali <alikhll@outlook.com>
2018-07-30 15:25:59 +0430

    container import modules enhanced

--- backend/implementation/container.ts
+++ backend/implementation/container.ts
@@ -1,6 +1,6 @@
 import * as srv from "../service/userService";
 import { Context } from "../model/context"
-import { Container } from "../node_modules/inversify";
+import { Container } from "inversify";
 
 module.exports = (container: Container) => {
     container.bind<Context>('Context').to(Context).inRequestScope();
