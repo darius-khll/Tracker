84890d4fedb2c517ceec97b7bbe0d07beb615967
ali <alikhll@outlook.com>
2018-07-31 21:01:17 +0430

    container enhanced

--- backend/implementation/container.ts
+++ backend/implementation/container.ts
@@ -3,7 +3,12 @@ import { Context } from "../model/context"
 import { Container } from "inversify";
 
 module.exports = (container: Container) => {
-    container.bind<Context>('Context').to(Context).inRequestScope();
+    container.bind<Context>('Context').to(Context).inRequestScope().onActivation((context, con) => {
+        con.connect();
+        return con;
+    });
+
+
     container.bind<srv.UserService>('UserService').to(srv.UserService).inRequestScope();
 }
 
