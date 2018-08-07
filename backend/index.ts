be29efec26ef0bba4cdab89757b3594bb8d66ac2
ali <alikhll@outlook.com>
2018-08-07 19:32:41 +0430

    index enhanced

--- backend/index.ts
+++ backend/index.ts
@@ -1,13 +1,15 @@
 import "reflect-metadata"; //should be at first of line
+import * as express from "express"
 import { InversifyExpressServer } from 'inversify-express-utils';
 import * as bodyParser from "body-parser";
 import { Container } from "inversify";
+import * as passport from "passport"
 
 //const datas = require("./app/data/data.json");
 //log(morgan, winston)
-//orm, di ioc(awilix)(InversifyJS), test(jasmine, mocha, selenium, Artillery(load test))
+//orm, di ioc(awilix)(InversifyJS), test(jest, jasmine, mocha, selenium, Artillery(load test))
 //sequelizejs orm, mongoose, jwt, forever,
-//dateTimeOffset, tslint, blocking, swagger, generator, rx, clustering, lodash, socket, GraphQL
+//dateTimeOffset, tslint, blocking, swagger, generator, rx, clustering, lodash, socket, GraphQL, passport
 //multer (multipart/form-data), JXcore
 
 let container = new Container();
@@ -15,19 +17,42 @@ require("./implementation/container")(container);
 
 let server = new InversifyExpressServer(container);
 
+declare global {
+    namespace Express {
+      interface Request {
+        session: any
+      }
+    }
+  }
+
 server.setConfig((app) => {
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use(bodyParser.json());
 
+    require("./implementation/passport")(app);
     require('./implementation/log')(app);
 
-    app.get("/", (req, res, next) => {
-        res.status(500);
+    app.get("/", (req: express.Request, res, next) => {
+
+        let u = req.session.passport; //retrun object saved in cookie and deserialize that
+
+        if (req.session.token) {
+            res.cookie('token', req.session.token);
+            res.json({
+                status: 'session cookie set'
+            });
+        } else {
+            res.cookie('token', '')
+            res.json({
+                status: 'session cookie not set'
+            });
+        }
+        //res.status(500);
     })
+
 });
 
 require("./implementation/route");
-
 
 let app = server.build();
 app.listen(3000, () => { console.log("start!") });
