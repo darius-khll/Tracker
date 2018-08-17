6f929b5cfea5ac57b8b51f0ec517481230347b13
alious <aliyr7592@gmail.com>
2018-08-18 00:30:27 +0430

    animation completed

--- frontend/src/components/Welcome.js
+++ frontend/src/components/Welcome.js
@@ -14,11 +14,20 @@ class Welcome extends Component{
     }
 
     loginFormUpdate() {
-        this.setState(
-            {
-            isSignUpForm : false
-            }
-        )
+        if(this.state.isSignUpForm){
+            this.setState(
+                {
+                isSignUpForm : false
+                }
+            )
+        }else{
+            this.setState(
+                {
+                isSignUpForm : true
+                }
+            ) 
+        }
+        
     }
 
     render() {
@@ -26,17 +35,64 @@ class Welcome extends Component{
             <div className="container-fluid page">
                 <CSSTransition 
                     in={this.state.isSignUpForm} 
-                    timeout={300} 
-                    classNames="login" 
-                    onExited={() => {
-                    this.setState({
-                        isSignUpForm: false,
-                    });
-                }}>
-                    <div className="row justify-content-center">
+                    timeout={1000} 
+                    classNames="signup">
+                    <div className="row justify-content-center main">
                         <div className="col col-md-6 col-sm-8 col-xs-12 align-items-center">
                             <div className=" registration-main-box">
                                 <h2 className="header">Sign Up</h2>
+                                <div className="text-input-container">
+                                    <input id="username" className="text-input" type="text" placeholder="Username" autocomplete="off"/>
+                                    <label htmlFor="username"></label>
+                                </div>
+                                <div className="text-input-container">
+                                    <input id="email" className="text-input" type="email" placeholder="Email" autocomplete="off"/>
+                                    <label htmlFor="email"></label>
+                                </div>
+                                <div className="text-input-container">
+                                    <input id="password" className="text-input" type="password" placeholder="Password" autocomplete="off"/>
+                                    <label htmlFor="password"></label>
+                                </div>
+                                <div className="text-input-container">
+                                    <div className="row justify-content-center">
+                                        <div className="col col-md-6 col-sm-6 col-xs-12 justify-content-center">
+                                            <button className="primary-button">Sign up</button>
+                                        </div>
+                                        <div className="col col-md-6 col-sm-6 col-xs-12 content-box">
+                                            Already have an account? 
+                                            <span onClick={this.loginFormUpdate}>Login</span>  
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="another-login-ways-container">
+                                <p>Sign Up with social platforms</p>
+                                <div className="row social-platforms-container">
+                                    <div className="col">
+                                        <span className="fa fa-google-plus"></span>
+                                    </div>
+                                    <div className="col">
+                                        <span className="fa fa-twitter"></span>
+                                    </div>
+                                    <div className="col">
+                                        <span className="fa fa-facebook"></span>
+                                    </div>
+                                    <div className="col">
+                                        <span className="fa fa-linkedin"></span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div> 
+                </CSSTransition>
+                <CSSTransition 
+                    in={!this.state.isSignUpForm} 
+                    timeout={1000} 
+                    classNames="login">
+                    <div className="row justify-content-center main">
+                        <div className="col col-md-6 col-sm-8 col-xs-12 align-items-center">
+                            <div className=" registration-main-box">
+                                <h2 className="header">Log In</h2>
                                 <div className="text-input-container">
                                     <input id="username" className="text-input" type="text" placeholder="Username" autocomplete="off"/>
                                     <label htmlFor="username"></label>
